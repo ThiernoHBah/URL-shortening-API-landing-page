2d01ec2153f0c77b2d41027e773831430297529a
thiernohbah <thiernobah2001@gmail.com>
2025-12-02 00:01:42 -0500

    forgot to commit changes

--- script.js
+++ script.js
@@ -13,3 +13,30 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const url = input.value;
+
+  shortenUrl(url);
+});
+  async function shortenUrl(url) {
+  try {
+    const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`);
+    const data = await res.json();
+    const newUrl = document.createElement("div");
+    newUrl.classList.add("item");
+    newUrl.innerHTML = `
+   <p> ${data.result.short_link}</p>
+   <button class="newUrl-btn" >Copy</button>
+   `;
+    result.prepend(newUrl);
+    const copyBtn = result.querySelector(".newUrl-btn");
+    copyBtn.addEventListener("click", () => {
+      navigator.clipboard.writeText(copyBtn.previousElementSibling.textContent);
+    });
+    input.value = "";
+  } catch (err) {
+    console.log(err);
+  }
+}
